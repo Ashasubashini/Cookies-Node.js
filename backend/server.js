8e50a295f5addbc009b2c35cd1408d7dc7716a8c
Ashasubashini <ashasubashini2005@gmail.com>
2025-07-21 12:43:12 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -209,23 +209,6 @@ app.get('/health', (req, res) => {
   res.status(200).json({ status: 'OK' });
 });
 
-pool.connect()
-  .then(client => {
-    console.log("Database connected successfully");
-    client.release();
-
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch(err => {
-    console.error("Failed to connect to the database:", err);
-    process.exit(1);
-  });
-
-
-
 
 
 module.exports = app; 
