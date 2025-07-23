600aaa3a20e06d4a058136910843832bf05f82fe
Ashasubashini <ashasubashini2005@gmail.com>
2025-07-23 09:06:18 +0530

    added valid access token

--- backend/testfiles/verify.test.js
+++ backend/testfiles/verify.test.js
@@ -3,7 +3,7 @@ const app = require('../server');
 
 describe('GET /verify - PASS', () => {
   it('should verify user if token is valid', async () => {
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImVkYTUwZDQyLWU2NzktNGU3Ni05OWNiLTAxYzFiZmZjN2Y3MCIsIm5hbWUiOiJhc2hhIiwiZW1haWwiOiJhc2hhQGVtYWlsLmNvbSIsImlhdCI6MTc1MzIxMTEyMCwiZXhwIjoxNzUzMjE0NzIwfQ.8v09Yhbrg1TLkrj_IvAI1JBZk2kB_wbMLOYphXSeIJY'; 
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImVkYTUwZDQyLWU2NzktNGU3Ni05OWNiLTAxYzFiZmZjN2Y3MCIsIm5hbWUiOiJhc2hhIiwiZW1haWwiOiJhc2hhQGVtYWlsLmNvbSIsImlhdCI6MTc1MzI0MTcyMywiZXhwIjoxNzUzMjQ1MzIzfQ.pf3H1IdCjGRh_j1d6PZIKua09QBgSEpd12fjbsp6o-w'; 
     const res = await request(app)
       .get('/verify')
       .set('Authorization', `Bearer ${token}`);
